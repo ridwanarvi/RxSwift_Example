a3692abab144ad5d840d96c7e740593f8242a8e8
Ridwan Arvihafiz <ridwanarvi@gmail.com>
2019-11-06 21:03:40 +0700

    add playground

--- RxPlayground.playground/Contents.swift
+++ RxPlayground.playground/Contents.swift
@@ -2,4 +2,36 @@ import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 import RxSwift
 
-let number = Observable<String>.of("satu")
+let disposeBag: DisposeBag = DisposeBag()
+let number: Observable<[String]> = Observable<[String]>.of(["satu", "dua"], ["tiga", "empat", "lima"], ["enam"])
+
+number.subscribe(onNext: { value in
+        print(value)
+    }, onError: { error in
+        print(error)
+    }, onCompleted: {
+        print("complete")
+    }, onDisposed: {
+        print("disposed")
+    }
+).disposed(by: disposeBag)
+
+
+let numberSubject: PublishSubject<String> = PublishSubject<String>()
+
+numberSubject.subscribe(onNext: { value in
+        print("subject \(value)")
+    }, onError: { error in
+        print(error)
+    }, onCompleted: {
+        print("complete subject")
+    }, onDisposed: {
+        print("disposed subject")
+    }
+).disposed(by: disposeBag)
+
+numberSubject.onNext("satu")
+print("wkwk - hihihi")
+numberSubject.onNext("dua")
+numberSubject.onCompleted()
+numberSubject.onNext("lima")
